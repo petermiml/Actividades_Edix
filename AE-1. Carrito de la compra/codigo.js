099e2eb4eadf489b2187b05b047fb460c479c527
ahmed <ahmed_daali@hotmail.es>
2022-10-21 14:12:20 +0100

    Corrección, ajuste y documentación del código

--- AE-1. Carrito de la compra/codigo.js
+++ AE-1. Carrito de la compra/codigo.js
@@ -1,4 +1,4 @@
-// =============================== Declaración de variables globales ==================================
+// ========================= Declaración de variables globales =================
 
 var nombre_art;
 var precio_art;
@@ -15,9 +15,19 @@ var valoresAceptados = /^[0-9]*(\.?)[0-9]+$/;
 var mostrarFormuTarjeta;
 var mostrarFormuEfectivo;
 var importeEfectivo;
-// =====================================================================================================
 
-// ============================= Función inicializadora de variables ===================================
+// ========================== Principal ========================================
+
+window.addEventListener("load",init);
+function init(){
+    initVariables();
+    initEventos();
+    ocultarDatosTarjeta();
+    ocultarDatosEfectivo();
+    mostrarImporteEfectivo();
+}
+
+// ======================== Función inicializadora de variables =================
 
 function initVariables(){
     nombre_art = document.getElementById("nombre_articulo");
@@ -39,9 +49,7 @@ function initVariables(){
 
 }
 
-// ====================================================================================================
-
-// =============================== Función inicializadora de eventos ==================================
+// ========================== Función inicializadora de eventos ================
 
 function initEventos(){
     btnAddCarrito.addEventListener("click", sumarPrecioCarrito);
@@ -50,52 +58,32 @@ function initEventos(){
     forma_pago.addEventListener("click",escribirFormulario);
     acepto_condiciones.addEventListener("change", permisoImpresion);
 }
-// =====================================================================================================
 
-// ========================================= Resto de funciones ========================================
+// =========================== Resto de funciones ===============================
 
-function ocultarDadatosTarjeta(){
-    mostrarFormuTarjeta.style.display = 'none';
-}
-function ocultarDatosEfectivo(){
-    mostrarFormuEfectivo.style.display ='none';
-}
-function escribirFormulario(){
-    if(forma_pago.value =="tarjeta"){
-        mostrarFormuTarjeta.style.display ='block';
-        mostrarFormuEfectivo.style.display ='none';
-    }
-    else if(forma_pago.value =="efectivo"){
-        mostrarFormuEfectivo.style.display ='block';
-        mostrarFormuTarjeta.style.display = 'none';
-        mostrarImporteEfectivo();
-    }else{
-        mostrarFormuEfectivo.style.display ='none';
-        mostrarFormuTarjeta.style.display = 'none';
-    }
-}
-function mostrarImporteEfectivo(){
-    importeEfectivo.value = precio_total.value;
-}
-// ------------------------------------------- Pedro Gómez --------------------------------------------
+// --------------------------- Pedro Gómez --------------------------------------
 // Primer punto
 
 function sumarPrecioCarrito(){
     var pre_tot = parseFloat(precio_total.value);
     var precioNumerico = false;
 
-    // Si el nombre del artículo está vacío escribe en la pantalla que falta el artículo, si no, deja vacío el aviso.
+    /* Si el nombre del artículo está vacío escribe en la pantalla que falta 
+    el artículo, si no, deja vacío el aviso.*/
     if(nombre_art.value == ''){
         aviso1.innerHTML = " &nbsp&nbspFalta artículo.";
     }else{
         aviso1.innerHTML = "";
     }
 
-    /* Si el precio del artículo está vacío escribe en la pantalla que falta el artículo, si no, lo deja vacío.
-        Si el valor introducido no es un número, avisa de que el dato debe ser numérico.
-        Si el precio es inferior a 0, avisará de que no puede ser inferior a 0 (igual a 0 si, debido a un posible
-        descuento).
-        Si ninguna de las condiciones anteriores se cumple, quitará el aviso.*/
+    /* Si el precio del artículo está vacío escribe en la pantalla que falta el
+     artículo, si no, lo deja vacío.
+    * Si el valor introducido no es un número, avisa de que el dato debe ser 
+     numérico.
+    * Si el precio es inferior a 0, avisará de que no puede ser inferior a 0 
+    (igual a 0 si, debido a un posible
+    descuento).
+    * Si ninguna de las condiciones anteriores se cumple, quitará el aviso.*/
 
     if(precio_art.value == ''){
         aviso2.innerHTML = " &nbsp&nbspFalta precio.";
@@ -108,17 +96,21 @@ function sumarPrecioCarrito(){
         aviso2.innerHTML = "&nbsp&nbspEl precio debe ser numérico.";  
     }
 
-    // Si las unidades son inferiores o iguales a 0, escribe en la pantalla que el número de unidades no puede ser 0 o negativo.
+    /* Si las unidades son inferiores o iguales a 0, escribe en la pantalla 
+    que el número de unidades no puede ser 0 o negativo.*/
     if(unidades.value <= 0){
         aviso3.innerHTML = " &nbsp&nbspEl número de unidades no puede ser menor o igual a 0.";
     }else{
         aviso3.innerHTML = "";
     }
 
-    /*  Si el nombre del articulo no está vacío, el precio es numérico y las unidades son superiores o iguales a 1:
-        multiplicara el precio del articulo por el de unidades y se lo sumará al precio total. Posteriormente, 
-        mostrará el precio total redondeado a dos decimales. Además introducirá en el campo del carrito los artículos
-        añadidos separados por comas si son mas de uno.*/
+    /*  Si el nombre del articulo no está vacío, el precio es numérico y las 
+    unidades son superiores o iguales a 1:
+    multiplicara el precio del articulo por el de unidades y se lo sumará al 
+    precio total.
+    Posteriormente, mostrará el precio total redondeado a dos decimales. 
+    Además introducirá en el campo del carrito los artículos añadidos separados
+    por comas si son mas de uno.*/
 
     if(nombre_art.value != '' && precioNumerico == true & parseInt(unidades.value) >= 1){
         aviso1.innerHTML = '';
@@ -141,9 +133,63 @@ function sumarPrecioCarrito(){
     }
 }
 
-// ------------------------------------------- Pedro --------------------------------------------
-// Cuando se pulse el botón "imprimir" debe mostrar a través de una ventana, tanto la lista de la compra final como el precio final del carrito según muestra la imágen.
-// Si no se ha seleccionado forma de pago, deberá aparecer un mensaje "seleccione método de pago".
+// ------------------------------------------- Ahmed ------------------------------------------
+// Segundo punto. Formas de pago.
+
+// Quedarán ocultas  las dos tablas de entrada. 
+function ocultarDatosTarjeta(){
+    mostrarFormuTarjeta.style.display = 'none';
+}
+function ocultarDatosEfectivo(){
+    mostrarFormuEfectivo.style.display ='none';
+}
+/* Funcion para rellenar los datos de pago.
+* Si se selecciona “Tarjeta” aparecerán tres nuevas cajas de texto para introducir 
+los datos de la 
+tarjeta bancaria.
+* Si se selecciona “Efectivo” aparecerá una nueva caja de texto con el importe 
+total del carrito.
+*/ 
+function escribirFormulario(){
+    if(forma_pago.value =="tarjeta"){
+        mostrarFormuTarjeta.style.display ='block';
+        mostrarFormuEfectivo.style.display ='none';
+    }
+    else if(forma_pago.value =="efectivo"){
+        mostrarFormuEfectivo.style.display ='block';
+        mostrarFormuTarjeta.style.display = 'none';
+        mostrarImporteEfectivo();
+    }else{
+        mostrarFormuEfectivo.style.display ='none';
+        mostrarFormuTarjeta.style.display = 'none';
+    }
+}
+function mostrarImporteEfectivo(){
+    importeEfectivo.value = precio_total.value;
+}
+// ------------------------------------------- Dani --------------------------------------------
+/* Tercer punto
+* El botón “Imprimir” se habilitará cuando se acepten las condiciones de compra.
+*/
+function permisoImpresion(){
+    
+    if(acepto_condiciones.checked == true){
+        
+        btnImprimir.disabled = false;
+    }else{
+        
+        btnImprimir.disabled = true;
+    }
+    }
+
+// ------------------------------------------- Pedro García------------------------------------------
+/* Cuarto punto
+
+* Cuando se pulse el botón "imprimir" debe mostrar a través de una ventana, 
+tanto la lista de la compra final como el precio final del carrito según
+muestra la imágen.
+* Si no se ha seleccionado forma de pago, deberá aparecer un mensaje 
+"seleccione método de pago".*/
 
 function imprimir(){
     if(forma_pago.value == "seleccione"){
@@ -158,9 +204,13 @@ function imprimir(){
 
 }
 
-
 // ------------------------------------------- Dani --------------------------------------------
-// Quinto punto
+/* Quinto punto
+*Con el botón “Restablecer” se resetean todas las cajas de texto para poder
+ introducir un nuevo producto. La caja para el nombre del artículo recibe 
+ el foco para facilitar la entrada de datos. Inicializa la caja “Precio total 
+ del carrito” a 0 para facilitar las operaciones aritméticas.
+*/
 function restab(){
     nombre_art.value = '';
     precio_art.value = '';
@@ -172,33 +222,3 @@ function restab(){
     aviso3.innerHTML = "";    
     nombre_art.focus();  
 }
-
-
-// ------------------------------------------- Ahmed --------------------------------------------
-// Segundo y tercer punto
-
-function permisoImpresion(){
-    
-    if(acepto_condiciones.checked == true){
-        
-        btnImprimir.disabled = false;
-    }else{
-        
-        btnImprimir.disabled = true;
-    }
-    }
-
-// =====================================================================================================
-
-// =========================================== Principal ===============================================
-
-window.addEventListener("load",init);
-function init(){
-    initVariables();
-    initEventos();
-    ocultarDadatosTarjeta();
-    ocultarDatosEfectivo();
-    mostrarImporteEfectivo();
-}
-
-// =====================================================================================================
